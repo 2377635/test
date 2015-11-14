ad50a9d6629710ec5a256abfb652d151416a8e91
ilya <2377635@gmail.com>
2015-11-14 11:42:46 +0300

    доделки просмотра документа

--- views/documents/view.php
+++ views/documents/view.php
@@ -7,16 +7,30 @@ use yii\helpers\Html;
 
 ?>
 
-<div class="documents-view">
-    <h3><?= Html::encode($document->title); ?></h3>
-    <div class="description"><?= Html::encode($document->description); ?></div>
-    <div>
-        <h5>Файлы</h5>
-        <ul>
-        <?php foreach ($attachments as $attachment) { ?>
-            <li><?= Html::a($attachment->origname, ['attachments/view', 'id' => $attachment->id]); ?></li>
-        <?php } ?>
-        </ul>
+<div class="documents-view row">
+    <div class="col-lg-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><?= Html::encode($document->title); ?></h3>
+            </div>
+            <div class="panel-body"><?= Html::encode($document->description); ?></div>
+        </div>
+        <?= Html::a('Вернуться', ['documents/index'], ['class' => 'btn btn-info']); ?>
     </div>
-    <?= Html::a('Вернуться', ['documents/index'], ['class' => 'btn btn-info']); ?>
+    
+    <div class="col-lg-4">
+        <table class="table table-striped">
+            <?php foreach ($attachments as $attachment) { ?>
+            <tr>
+                <td>
+                    <?= $attachment->origname; ?>
+                </td>
+                <td>
+                    <?= Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['attachments/view', 'id' => $attachment->id], ['target' => '_blank']); ?>
+                </td>
+            </tr>
+            <?php } ?>
+        </table>
+    </div>
+    
 </div>
